90b17d8de4e721f135bbac10b744c63e65ae2f0c
maddy95sharma <maddy95sharma@gmail.com>
2023-04-10 11:47:07 +0530

    new commit

--- Test.cs
+++ Test.cs
@@ -1,9 +1,14 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 <<<<<<< HEAD
 using System;
 =======
 using UnityEngine;
 >>>>>>> 82ff8b1eec0923ba9a65d3f40b081b22899d3780
+=======
+using System;
+using UnityEditor;
+>>>>>>> Stashed changes
 =======
 using System;
 using UnityEditor;
